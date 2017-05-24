0d2f763468be6ee462b5d1b7cf5b47006f8c5c20
amatanat <ahmadova.matanat01@gmail.com>
2017-05-24 15:16:11 +0200

    News app

---
+++ ReadNews/app/src/main/java/com/am/readnews/MainActivity.java
@@ -0,0 +1,15 @@
+package com.am.readnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String API_KEY = "84e62d46-a76e-49d0-a15f-cc05c61fba1a";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
