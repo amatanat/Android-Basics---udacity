d5046784b4a920c8cb953899ca6f69fa18c769ee
amatanat <a_metanet@hotmail.com>
2017-05-26 10:01:49 +0200

    Loader

---
+++ ReadNews/app/src/main/java/com/am/readnews/NewsLoader.java
@@ -0,0 +1,38 @@
+package com.am.readnews;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by amatanat on 26.05.17.
+ */
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+    private String mUrl;
+
+    public NewsLoader(Context context, String url){
+        super(context);
+        mUrl = url;
+    }
+
+    protected void onStartLoading(){
+        forceLoad();
+    }
+
+    @Override
+    public List<News> loadInBackground() {
+
+        // check if string url is null or not
+        if (mUrl == null){
+            return null;
+        }
+
+        // get list of {@link News} from given url
+        List<News> newsList = Query.fetchData(mUrl);
+
+        return newsList;
+    }
+}
