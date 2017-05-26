792c2358cb5bc0bcf12fa26ba04a0ca009d93119
amatanat <a_metanet@hotmail.com>
2017-05-26 10:02:23 +0200

    Update MainActivity for string URL

--- ReadNews/app/src/main/java/com/am/readnews/MainActivity.java
+++ ReadNews/app/src/main/java/com/am/readnews/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // String url to get data from internet
+    private final String STRING_URL = "http://content.guardianapis.com/search?section=technology&api-key=84e62d46-a76e-49d0-a15f-cc05c61fba1a";
+
+    // my api_key
     private final String API_KEY = "84e62d46-a76e-49d0-a15f-cc05c61fba1a";
 
     @Override
