1126464c9259ffc73959d9f66409ac74485699ea
amatanat <a_metanet@hotmail.com>
2017-06-30 09:45:38 +0200

    Add radio button and checkbox in layout xml file

--- QuizApp/app/src/main/java/com/am/quizapp/QuizActivity.java
+++ QuizApp/app/src/main/java/com/am/quizapp/QuizActivity.java
@@ -2,6 +2,8 @@ package com.am.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class QuizActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_quiz);
+  }
+
+/*  public void onRadioButtonClicked(View view) {
+    // Is the button now checked?
+    boolean checked = ((RadioButton) view).isChecked();
+
+    // Check which radio button was clicked
+    switch(view.getId()) {
+      case R.id.true_radiobutton:
+        if (checked)
+          // true is checked
+          break;
+      case R.id.false_radiobutton:
+        if (checked)
+          // false is checked
+          break;
+    }
+  }*/
+
+  public void submitAnswers(){
+
   }
 }
